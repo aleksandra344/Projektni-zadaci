8c50b160497ffdc65948a12b0ec0a54f9b0799f2
AleksandraBlažeković <93856657+aleksandra344@users.noreply.github.com>
2025-01-16 16:45:53 +0100

    ZADATAK 1 - ourses.php

---
+++ courses.php
@@ -0,0 +1,2 @@
+<h1>Our Courses</h1>
+<p>Check out the amazing courses we offer to unleash your creativity. Whether you're a beginner or experienced, our expert instructors are here to guide you every step of the way.</p>
