a81e8e4de8e79f27334d161dcfecb213fbdd622b
AleksandraBlažeković <93856657+aleksandra344@users.noreply.github.com>
2025-01-16 16:43:48 +0100

    ZADATAK 1 - footer.php

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+    <p>Copyright &copy;2024 <a href="https://github.com/aleksandra344">Aleksandra Blažeković <img src="photos/github2.jpg" title="Github"></a></p>
+</footer>
