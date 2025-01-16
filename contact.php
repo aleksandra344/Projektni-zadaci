64afcc5c35420e643b40f9429bac208e16e8935f
AleksandraBlažeković <93856657+aleksandra344@users.noreply.github.com>
2025-01-16 16:45:24 +0100

    ZADATAK 1 - contact.php

---
+++ contact.php
@@ -0,0 +1,14 @@
+<h1>Contact Us</h1>
+<p>Have questions or need more information? Get in touch with us through the form below or via email, and we'll get back to you as soon as possible.</p>
+<form action="submit_contact.php" method="POST">
+    <label for="name">Name:</label><br>
+    <input type="text" id="name" name="name" required><br><br>
+    
+    <label for="email">Email:</label><br>
+    <input type="email" id="email" name="email" required><br><br>
+    
+    <label for="message">Message:</label><br>
+    <textarea id="message" name="message" required></textarea><br><br>
+    
+    <input type="submit" value="Send Message">
+</form>
