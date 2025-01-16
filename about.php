edb4c67ed9a4a391b36de118231d612f132bcad3
AleksandraBlažeković <93856657+aleksandra344@users.noreply.github.com>
2025-01-16 16:44:55 +0100

    ZADATAK 1 - about.php

---
+++ about.php
@@ -0,0 +1,2 @@
+<h1>About Us</h1>
+<p>We are a workshop offering clay and wine experiences. Our mission is to provide a relaxing and creative atmosphere for people to explore their artistic side while enjoying a glass of wine.</p>
