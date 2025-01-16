aa9205da455c2b94153b698b2f19e11b327dae51
AleksandraBlažeković <93856657+aleksandra344@users.noreply.github.com>
2025-01-16 16:44:20 +0100

    ZADATAK 1 - home.php

---
+++ home.php
@@ -0,0 +1,34 @@
+<section class="header">
+    <div class="text-box">
+        <h1>Because modeling clay is easier with a glass of wine</h1>
+        <h2>New dates are out!</h2>
+        <p>Shop below.</p>
+        <a href="index.php?menu=4" class="hero-btn">Shop all</a>
+    </div>
+</section>
+
+<!--campus-->
+<section class="campus">
+    <h1>You can find us anywhere!</h1>
+    <p>Your kind support gave us an opportunity to spread into three distinct cities.</p>
+    <div class="row">
+        <div class="campus-colour">
+            <img src="photos/zgrada1.jpg" alt="Zagreb">
+            <div class="layer">
+                <h3>ZAGREB</h3>
+            </div>
+        </div>
+        <div class="campus-colour">
+            <img src="photos/zgrada2.jpg" alt="Licki Osik">
+            <div class="layer">
+                <h3>LIČKI OSIK</h3>
+            </div>
+        </div>
+        <div class="campus-colour">
+            <img src="photos/zgrada3.jpg" alt="Vinkovci">
+            <div class="layer">
+                <h3>VINKOVCI</h3>
+            </div>
+        </div>
+    </div>
+</section>
