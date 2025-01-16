54110631ab519da5fd70672466a7c2eec9bd0cab
AleksandraBlažeković <93856657+aleksandra344@users.noreply.github.com>
2025-01-16 16:46:35 +0100

    ZADATAK 1 - products.php

---
+++ products.php
@@ -0,0 +1,2 @@
+<h1>Our Products</h1>
+<p>Browse through our exclusive collection of clay and wine products. From handcrafted ceramics to unique gifts, you'll find the perfect item to take home or give to a loved one.</p>
